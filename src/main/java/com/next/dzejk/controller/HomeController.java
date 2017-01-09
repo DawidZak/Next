c81b7ff9e57980121b84d79cd2d97ba3a678339b
Dawid Żak <dawix64@gmail.com>
2017-01-09 18:31:26 +0100

    Test

--- src/main/java/com/next/dzejk/controller/HomeController.java
+++ src/main/java/com/next/dzejk/controller/HomeController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -27,7 +26,6 @@ import com.next.dzejk.services.IUserService;
  * Handles requests for the application home page.
  */
 @Controller 
-//@Scope("request")
 
 public class HomeController {
 
